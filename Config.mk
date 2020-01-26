66c04a936102886e5681f3273db0b6588c47e963
Mikael Kalms <mikael@kalms.org>
2020-01-26 16:12:44 +0100

    Imported nightly/release support from vasmm68k

--- Config.mk
+++ Config.mk
@@ -1,3 +1,4 @@
-VLINK_URL = http://server.owl.de/~frank/tags/vlink0_16a.tar.gz
+VLINK_NIGHTLY_URL = http://sun.hasenbraten.de/vlink/daily/vlink.tar.gz
+VLINK_RELEASE_URL = http://server.owl.de/~frank/tags/vlink0_16a.tar.gz
 VLINK_DOC_URL = http://sun.hasenbraten.de/vlink/release/vlink.pdf
-VLINK_VERSION = 0.16a
+VLINK_RELEASE_VERSION = 0.16a
